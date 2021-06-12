6bba2648d0096935dfee397691f7c4c55769ef40
S Nitesh Singh <niteshseram@gmail.com>
2021-06-12 10:03:08 +0530

    initial commit

---
+++ src/themes/default.js
@@ -0,0 +1,20 @@
+export default {
+  // Temp fonts
+  fonts: {
+    title: "Space Grotesk, sans-serif",
+    main: "Space Grotesk, sans-serif",
+  },
+  // Colors for layout
+  colors: {
+    bg: "#07111A",
+    text: "#fff",
+  },
+  // Breakpoints for responsive design
+  breakpoints: {
+    xs: "screen and (max-width: 480px)",
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (max-width: 768px)",
+    lg: "screen and (max-width: 1024px)",
+    xl: "screen and (max-width: 1280px)",
+  },
+};
