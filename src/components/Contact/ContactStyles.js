39bf3bbb79a0ca6ceb654bdd7435b0cbc5713e24
S Nitesh Singh <niteshseram@gmail.com>
2021-06-14 11:01:19 +0530

    changing Contact section height

--- src/components/Contact/ContactStyles.js
+++ src/components/Contact/ContactStyles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-  height: 100vh;
+  height: 88vh;
   display: flex;
   align-items: center;
   justify-content: center;
